3de56e9fb931af498fe733d43ba10121acf97ed6
Jennifer Naasz <naasz.jl@gmail.com>
2017-05-01 21:38:13 -0400

    to-do list done

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 var app = angular.module('ourModule', []);
 
 app.controller('ourController', function($scope){
-  $scope.toDoList = [];
-  $scope.addTask = function(newTask){
-    console.log(newTask);
-    $scope.toDoList.push({ name: newTask.name});
-  }
 
+  $scope.toDoListMonday = [];
+  $scope.addTaskMonday = function(mon){
+    console.log(mon);
+    $scope.toDoListMonday.push({ name: mon.name});
+  };
+
+  $scope.toDoListTuesday = [];
+  $scope.addTaskTuesday = function(tue){
+    console.log(tue);
+    $scope.toDoListTuesday.push({ name: tue.name});
+  };
+
+  $scope.toDoListWednesday = [];
+  $scope.addTaskWednesday = function(wed){
+    console.log(wed);
+    $scope.toDoListWednesday.push({ name: wed.name});
+  };
+
+  $scope.toDoListThursday = [];
+  $scope.addTaskThursday = function(thu){
+    console.log(thu);
+    $scope.toDoListThursday.push({ name: thu.name});
+  };
+
+  $scope.toDoListFriday = [];
+  $scope.addTaskFriday = function(fri){
+    console.log(fri);
+    $scope.toDoListFriday.push({ name: fri.name});
+  };
 
 
 })
